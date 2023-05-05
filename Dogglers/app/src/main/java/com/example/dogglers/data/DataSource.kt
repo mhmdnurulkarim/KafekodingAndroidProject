64dd623b8bf0e51f211962067f5119570e3dc19a
karim.2501 <karimmitra0@gmail.com>
2023-05-05 22:14:17 +0700

    Upload starter doggler

---
+++ Dogglers/app/src/main/java/com/example/dogglers/data/DataSource.kt
@@ -0,0 +1,16 @@
+
+package com.example.dogglers.data
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+/**
+ * An object to generate a static list of dogs
+ */
+@Parcelize
+data class DataSource(
+    val Photo: Int,
+    val Name: String,
+    val Age: Int,
+    val Hobbies: String
+): Parcelable
